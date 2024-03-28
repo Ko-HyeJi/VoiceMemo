d50a5a7974f60afa952ae7c25fca206679d94115
Ko-HyeJi <hy2ji.k5@gmail.com>
2024-03-29 05:29:06 +0900

    set directory structure

---
+++ VoiceMemo/VoiceMemo/App/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  VoiceMemo
+//
+//  Created by 고혜지 on 3/29/24.
+//
+
+import Foundation
---
+++ VoiceMemo/VoiceMemo/Core/Extension/Color+Extensions.swift
@@ -0,0 +1,11 @@
+//
+//  Color+Extensions.swift
+//  VoiceMemo
+//
+//  Created by 고혜지 on 3/29/24.
+//
+
+import SwiftUI
+
+extension Color {
+}
